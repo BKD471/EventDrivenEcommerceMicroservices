48424b4bd4309f2b39bd48c7479db14c13f35ca8
BKD471 <bhaskarkumardas9@gmail.com>
2025-12-02 12:15:16 +0530

    setting up the common services

---
+++ ecommerce-bom/common/src/main/java/com/forsaken/ecommerce/common/exceptions/CustomerNotFoundExceptions.java
@@ -0,0 +1,21 @@
+package com.forsaken.ecommerce.common.exceptions;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CustomerNotFoundExceptions extends Exception {
+
+    private final String message;
+    private final String methodName;
+
+    public CustomerNotFoundExceptions(final String message,
+                                      final String methodName
+    ) {
+        super(String.format("%s in %s", message, methodName));
+        this.message = message;
+        this.methodName = methodName;
+    }
+}
