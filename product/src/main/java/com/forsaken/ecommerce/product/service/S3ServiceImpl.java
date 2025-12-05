9892507d7cd85ac883646e69211d7d0cf611fd73
Bhaskar Kumar Das <bhaskarkumardas9@gmail.com>
2025-12-05 16:27:41 +0530

    Integrated Product Controller with Product Service & Setup Gateway Routes for Product (#18)

--- product/src/main/java/com/forsaken/ecommerce/product/service/S3ServiceImpl.java
+++ product/src/main/java/com/forsaken/ecommerce/product/service/S3ServiceImpl.java
@@ -1,20 +1,27 @@
 package com.forsaken.ecommerce.product.service;
 
 
+import com.forsaken.ecommerce.product.configs.s3.S3Properties;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.UUID;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class S3ServiceImpl implements IS3Service {
 
+    private final S3Properties s3Properties;
+    private final S3Presigner presigner;
 
     @Override
     public Map<String, String> generatePresignedUploadUrl(
@@ -25,12 +32,15 @@ public class S3ServiceImpl implements IS3Service {
         final String key = "uploads/" + UUID.randomUUID() + "_" + originalFilename;
 
         final PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket("// TODO give a Bucket Name") // TODO pass down a S3 bucket name
+                .bucket(s3Properties.bucketName())
                 .key(key)
                 .contentType(contentType)
                 .build();
 
-        final PresignedPutObjectRequest presignedRequest = null; // TODO Create a presigned put object request with expiration
+        final PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(builder ->
+                builder.signatureDuration(Duration.ofMinutes(s3Properties.expiration())) // URL valid for 30 mins
+                        .putObjectRequest(putObjectRequest)
+        );
         log.info("Presigned Upload URL: {}", presignedRequest.url());
 
         return Map.of(
@@ -44,11 +54,14 @@ public class S3ServiceImpl implements IS3Service {
         log.info("Generate Presigned Download URL for image: {}", imageUrl);
         final String key = imageUrl.substring(imageUrl.lastIndexOf("/uploads") + 1);
         final GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket("// TODO give a Bucket Name") // TODO pass down a S3 bucket name
+                .bucket(s3Properties.bucketName())
                 .key(key)
                 .build();
 
-        final PresignedGetObjectRequest presignedGet = null; // TODO Create a presigned put object request
+        final PresignedGetObjectRequest presignedGet = presigner.presignGetObject(builder ->
+                builder.signatureDuration(Duration.ofMinutes(s3Properties.expiration()))
+                        .getObjectRequest(getObjectRequest)
+        );
         log.info("Presigned Download URL: {}", presignedGet.url());
         return presignedGet.url().toString();
     }
